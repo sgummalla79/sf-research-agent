56b3608910c8c952a6c726fc065dc5faa51d7554
Suman Gummalla <sgummalla.work@gmail.com>
2026-05-13 14:05:37 -0700

    tweak: brighten sidebar Chakra icon from #c97040 to #f5a55a

--- frontend/src/components/ChatWindow.vue
+++ frontend/src/components/ChatWindow.vue
@@ -18,7 +18,7 @@
 
         <!-- Brand header -->
         <div class="sb-header">
-          <SudarshanChakra :size="22" color="#c97040" />
+          <SudarshanChakra :size="22" color="#f5a55a" />
           <span class="sb-app-name">Prajna</span>
           <button class="sb-collapse-btn" title="Collapse sidebar" @click="sidebar.open = false">
             <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round" width="18" height="18">
